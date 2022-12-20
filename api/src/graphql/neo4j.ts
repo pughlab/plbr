e30b5d00f0b35dc95bed34ab00871e13df483db4
carlsondultra <carlsondultra@gmail.com>
2022-12-19 20:46:51 -0500

    return integer instead of {high, low}

--- api/src/graphql/neo4j.ts
+++ api/src/graphql/neo4j.ts
@@ -11,5 +11,7 @@ export const driver = neo4j.driver(
     process.env.NEO4J_USER || 'neo4j',
     process.env.NEO4J_PASSWORD || 'neo4j'
   ),
-  { encrypted: "ENCRYPTION_OFF"}
+  { encrypted: "ENCRYPTION_OFF",
+   disableLosslessIntegers: true 
+  }
 )
