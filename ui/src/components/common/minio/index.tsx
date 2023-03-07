c98d1c5be1a706e3730d3d50a1dc951b6c590100
carlsondultra <carlsondultra@gmail.com>
2023-03-07 09:08:21 -0500

    changing design of dropzone under datasets

--- ui/src/components/common/minio/index.tsx
+++ ui/src/components/common/minio/index.tsx
@@ -1,16 +1,19 @@
-import React, { useCallback } from 'react'
+import React, { useCallback, useMemo, useState } from 'react'
 import { Route, Routes, useParams } from 'react-router-dom'
-import { Message, Divider, Container, Icon, List, Input, Segment, Form, Button, Modal } from 'semantic-ui-react'
+import { Message, Divider, Container, Header, Icon, List, Input, Segment, Form, Button, Modal } from 'semantic-ui-react'
 import SegmentPlaceholder from '../SegmentPlaceholder'
 import { useDropzone, FileWithPath } from 'react-dropzone'
 import { gql, useQuery } from '@apollo/client'
 import useMinioUploadMutation from '../../../hooks/useMinioUploadMutation'
-import SegmentPlaceholder from '../SegmentPlaceholder'
 
 
 function MinioUploadModal({ bucketName }) {
     const { state: uploadState, dispatch: uploadDispatch, mutation: uploadMutation } = useMinioUploadMutation()
+
+    const [testing, setTesting] = useState({})
+
     const onDrop = useCallback((files: FileWithPath[]) => {
+
         uploadMutation({
             variables: {
                 bucketName: bucketName,
@@ -18,23 +21,66 @@ function MinioUploadModal({ bucketName }) {
             }
         })
     }, [])
-    const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
+
+
+    const { getRootProps, getInputProps, isDragActive, isDragAccept,isDragReject,} = useDropzone({ onDrop })
+
+    //styling
+    const baseStyle = {
+        flex: 1,
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        padding: "20px",
+        borderWidth: 2,
+        borderRadius: 20,
+        borderColor: "#26C2E7",
+        borderStyle: "dashed",
+        backgroundColor: "#fafafa",
+        color: "#c4c4c4",
+        outline: "none",
+        transition: "border .24s ease-in-out"
+      };
+      
+      const activeStyle = {
+        borderColor: "#f2f"
+      };
+      
+      const acceptStyle = {
+        borderColor: "#00ff00"
+      };
+      
+      const rejectStyle = {
+        borderColor: "#fe0100"
+      };
+
+    const style = useMemo(
+        () => ({
+          ...baseStyle,
+          ...(isDragActive ? activeStyle : {}),
+          ...(isDragAccept ? acceptStyle : {}),
+          ...(isDragReject ? rejectStyle : {})
+        }),
+        [isDragActive, isDragReject, isDragAccept]
+      );
     return (
-        <Modal
-            trigger={<Button fluid icon='upload' />}
-        >
-            <Modal.Content>
-                <SegmentPlaceholder text='Click to upload a file' icon='upload'>
-                    <div {...getRootProps()}>
-                        <Button>
-                            Upload a file
-                            <input {...getInputProps()} />
-                        </Button>
-                    </div>
-                </SegmentPlaceholder>
-            </Modal.Content>
-        </Modal>
-    )
+        <section className="container">
+          <div {...getRootProps({ style })}>
+            <input {...getInputProps()} />
+    
+            {/* Dropzone Text*/}
+            <Header
+              content={"Drag 'n' drop some files here, or click to select files" }
+              textAlign="center"
+            />
+
+          </div>
+          <aside>
+            {/* <h4>Accepted files</h4>
+            <ol>{acceptedFileItems}</ol> */}
+          </aside>
+        </section>
+      );
 }
 
 export default function MinioBucket({ bucketName }) {
@@ -56,7 +102,7 @@ export default function MinioBucket({ bucketName }) {
             <MinioUploadModal bucketName={bucketName} />
             <Divider horizontal />
             {!minioUploads.length ? <SegmentPlaceholder text={'No uploads yet'} /> :
-                <List celled divided>
+                <List loading={loading} celled divided>
                     {minioUploads.map(minioUpload => (
                         <List.Item
                             content={minioUpload.filename}
